c1edfdb5e84f0a488e640e6818dedfd22c789280
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-08-01 12:03:06 +0200

    adding route for the program browse api

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -2,21 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const { sayWelcome } = require("../../controllers/sayActions");
-
-router.get("/", sayWelcome);
-
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+// Import item-related actions
+const { browse } = require("../../../controllers/programActions");
 
-router.use("/items", itemsRouter);
-
-const programRouter = require("./programs/router");
-
-router.use("/programs", programRouter);
+// Route to get a list of items
+router.get("/", browse);
 
 /* ************************************************************************* */
 
